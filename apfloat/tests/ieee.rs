149bb0b85155f2155bb9694e0ac1cc5e17fff292
Dr Maxim Orlovsky <orlovsky@ubideco.org>
2026-05-14 18:51:38 +0300

    chore: fix clippy warnings

--- apfloat/tests/ieee.rs
+++ apfloat/tests/ieee.rs
@@ -8,6 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#![allow(clippy::approx_constant)]
+
 #[macro_use]
 extern crate amplify_apfloat;
 extern crate amplify_num;
